ce6c776409ab43fc5d5317d4719c60cd40582498
sungazee <anasbaslih@gmail.com>
2024-03-12 16:26:28 +0100

    first step

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"app-api-484e6","appId":"1:39191817865:web:513f9edf2fe8526b1e2058","storageBucket":"app-api-484e6.appspot.com","apiKey":"AIzaSyCNkDOn5RCjEUHGggD-fIzx_Lt3tm4EuBk","authDomain":"app-api-484e6.firebaseapp.com","messagingSenderId":"39191817865"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"app-api-484e6","appId":"1:39191817865:web:513f9edf2fe8526b1e2058","storageBucket":"app-api-484e6.appspot.com","apiKey":"AIzaSyCNkDOn5RCjEUHGggD-fIzx_Lt3tm4EuBk","authDomain":"app-api-484e6.firebaseapp.com","messagingSenderId":"39191817865"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
 };
